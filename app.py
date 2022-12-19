2243b21623c5d7a6b6cb8086be14e35ad3973d0e
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-19 21:27:39 +0200

    Configures app, initializes database and creates tables

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask,request,json
-from flask_smorest import abort
+import os
 
-app = Flask(__name__)
+from flask import Flask
+from flask_smorest import Api
 
-@app.route('/')
-def hello():
-    return 'Hello'
+from resources.db import db
+import Models
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def create_app(db_url=None):
+
+    app = Flask(__name__)
+
+    app.config['PROPAGATE_EXCEPTIONS']=True
+    app.config['API_TITLE']='Stores REST API'
+    app.config['API_VERSION']='v1.0.0'
+    app.config['OPENAPI_VERSION']='3.0.3'
+    app.config['OPENAPI_URL_PREFIX']='/'
+    app.config['OPENAPI_SWAGGER_UI_PATH']='/docs/swagger'
+    app.config['OPENAPI_SWAGGER_UI_URL']='https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js'
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.__init__(app)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
+    api=Api(app)
+
+    return app
